f149adc17f29ed9e1b8b7b53c86ff8e9377ac7b0
ThamTT <115442362+ThamTT@users.noreply.github.com>
2023-04-01 19:48:27 +0700

    Init Project ATOS2023_R1

---
+++ project_ATOS2023_R01/src/test/java/testOpen.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class testOpen {
+    public static void main(String[] args) {
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://www.google.com.vn/?hl=vi");
+
+    }
+}
