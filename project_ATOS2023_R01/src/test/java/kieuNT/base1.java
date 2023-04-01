16d079a0cf88886f6fe41a3b1872d07d68b2ae6e
NTKieu <ntkieu27102k1@gmail.com>
2023-04-01 21:53:22 +0700

    java core1

---
+++ project_ATOS2023_R01/src/test/java/kieuNT/base1.java
@@ -0,0 +1,12 @@
+package kieuNT;
+
+public class base1 {
+    public static void main(String[] args) {
+        int tong =0;
+        for(int i=20; i<=30; i+=2){
+            tong += i;
+            System.out.println(" "+i);
+        }
+        System.out.println("Tong = "+tong);
+    }
+}
