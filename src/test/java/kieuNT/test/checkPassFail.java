57197ba1d78f5c9efd7cf322f9dfafe054fb6a6d
NTKieu <ntkieu27102k1@gmail.com>
2023-04-18 20:20:41 +0700

    update

--- src/test/java/kieuNT/test/checkPassFail.java
+++ src/test/java/kieuNT/test/checkPassFail.java
@@ -1,6 +1,5 @@
 package kieuNT.test;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+
 public class checkPassFail {
     public static String checkMark(int mark) {
         if(mark >= 50 && mark < 100)
