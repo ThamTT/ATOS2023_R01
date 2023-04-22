a82d11cc7e2e5ad201171a08f67611211166ab9b
DLanhhhhhh1234 <129610287+DLanhhhhhh1234@users.noreply.github.com>
2023-04-22 14:58:41 +0700

    btb10

---
+++ src/test/java/lanhD/testng/btvnBuoi10.java
@@ -0,0 +1,27 @@
+package lanhD.testng;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class btvnBuoi10 {
+    @Test
+    public void Test10() throws InterruptedException {
+        ChromeOptions options= new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver= new ChromeDriver(options);
+        driver.get("https://tiki.vn/");
+        driver.manage().window().maximize();
+
+        WebElement checkMeBe= driver.findElement(By.xpath("(//div[@title='Đồ Chơi - Mẹ & Bé'])/.."));
+        checkMeBe.click();
+        Thread.sleep(3000);
+        WebElement checkBoxGiamSau= driver.findElement(By.xpath("(//label[@class='style__CheckboxFake-sc-1ewjwu4-4 eUuyqO'])[4]/.."));
+        checkBoxGiamSau.click();
+
+
+    }
+}
