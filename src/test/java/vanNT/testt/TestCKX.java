2b217502152d92f0545c789aed24b4c8c9e0f081
VanTester <129405991+VanTester@users.noreply.github.com>
2023-04-22 08:41:30 +0700

    create TestCKX

---
+++ src/test/java/vanNT/testt/TestCKX.java
@@ -0,0 +1,44 @@
+package vanNT.testt;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class TestCKX {
+    @Test
+    public void TestCKX (){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://demos.jquerymobile.com/1.4.5/checkboxradio-checkbox/");
+        driver.manage().window().maximize();
+
+         WebElement inputCkx = driver.findElement(By.xpath("(//input[@type='checkbox'])[1]/.."));
+         inputCkx.click();
+
+
+        WebElement Agree = driver.findElement(By.xpath("(//label[text()='I agree'])[1]/.."));
+        Agree.click();
+
+        WebElement CkxMenu = driver.findElement(By.xpath("//a[normalize-space(text()) = 'Checkboxradio widget']\n"));
+        CkxMenu.click();
+
+        WebElement CkxRadio = driver.findElement(By.xpath("(//a[normalize-space(text()) = 'Radio buttons'])[1]"));
+        CkxRadio.click();
+
+        WebElement CkxOnee = driver.findElement(By.xpath("(//input[@id='radio-choice-0a'])/.."));
+        CkxOnee.click();
+
+        WebElement CkxTwo = driver.findElement(By.xpath("//input[@id='radio-choice-0b']/.."));
+
+
+        WebElement CkxOneee = driver.findElement(By.xpath("(//input[@id='radio-choice-0a'])"));
+        if (CkxOneee.isSelected())
+            CkxTwo.click();
+
+        driver.close();
+    }
+}
